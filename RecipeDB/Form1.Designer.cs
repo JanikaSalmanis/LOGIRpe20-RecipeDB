702db7fea7a9c3e9fc5cf5285f65c74aad634938
Opilane TTHK <opilane@tthk.ee>
2021-02-18 12:05:15 +0200

    okei

--- RecipeDB/Form1.Designer.cs
+++ RecipeDB/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RecipeDB
 {
-    partial class Form1
+    partial class GookBook
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.listRecipe = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listIngridient = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(12, 25);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(41, 13);
+            this.Label1.TabIndex = 0;
+            this.Label1.Text = "Recipe";
+            this.Label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listRecipe
+            // 
+            this.listRecipe.FormattingEnabled = true;
+            this.listRecipe.Location = new System.Drawing.Point(12, 62);
+            this.listRecipe.Name = "listRecipe";
+            this.listRecipe.Size = new System.Drawing.Size(144, 121);
+            this.listRecipe.TabIndex = 1;
+            this.listRecipe.SelectedIndexChanged += new System.EventHandler(this.listRecipe_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(308, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ingridient";
+            // 
+            // listIngridient
+            // 
+            this.listIngridient.FormattingEnabled = true;
+            this.listIngridient.Location = new System.Drawing.Point(298, 62);
+            this.listIngridient.Name = "listIngridient";
+            this.listIngridient.Size = new System.Drawing.Size(153, 121);
+            this.listIngridient.TabIndex = 3;
+            // 
+            // GookBook
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listIngridient);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listRecipe);
+            this.Controls.Add(this.Label1);
+            this.Name = "GookBook";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.GookBook_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.ListBox listRecipe;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listIngridient;
     }
 }
 
--- RecipeDB/Form1.cs
+++ RecipeDB/Form1.cs
@@ -7,14 +7,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace RecipeDB
 {
-    public partial class Form1 : Form
+    public partial class GookBook : Form
     {
-        public Form1()
+        string connectionstring;
+        SqlConnection connection;
+        public GookBook()
         {
             InitializeComponent();
+            connectionstring = ConfigurationManager.ConnectionStrings["RecipeDB.Properties.Settings.Database1ConnectionString"].ConnectionString;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GookBook_Load(object sender, EventArgs e)
+        {
+            PopulateRecipes();
+        }
+
+        private void PopulateRecipes()
+        {
+            using(connection = new SqlConnection(connectionstring))
+            using(SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Recipe",connection))
+            {
+                DataTable recipeTable = new DataTable();
+                adapter.Fill(recipeTable);
+
+                listRecipe.DisplayMember = "Name";
+                listRecipe.ValueMember = "Id";
+                listRecipe.DataSource = recipeTable;
+            }
+        }
+
+        private void PopulateIngredients()
+        {
+            string query = "SELECT Ingredient.Name FROM Ingredient JOIN RecipeIngredient ON Ingredient.Id = RecipeIngredient.IngredientId WHERE RecipeIngredient.RecipeId = @RecipeId";
+            using (connection = new SqlConnection(connectionstring))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                //get recipe id from the listRecipe listbox
+                command.Parameters.AddWithValue("@RecipeId", listRecipe.SelectedValue);
+                DataTable ingredientTable = new DataTable();
+                adapter.Fill(ingredientTable);
+
+                listIngridient.DisplayMember = "Name";
+                listIngridient.ValueMember = "Id";
+                listIngridient.DataSource = ingredientTable;
+            }
+        }
+
+        private void listRecipe_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PopulateIngredients();
         }
     }
 }
